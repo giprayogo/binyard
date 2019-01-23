a1609660f12837fc3c4dbc2cd197f7ddf7bd7af7
giprayogo <genki016@gmail.com>
2019-01-23 10:03:31 +0900

    Make error column optional argument and print reference row

--- quickbin/general_diff.py
+++ quickbin/general_diff.py
@@ -7,11 +7,14 @@ from fractions import Fraction
 try:
     filename = sys.argv[1]
     columns = list(map(int,sys.argv[2].split(',')))
-    err_columns = list(map(int,sys.argv[3].split(',')))
 except IndexError:
     #TODO: more general format
-    print("Usage: () FILENAME COLUMN ERR_COLUMN".format(sys.argv[0]))
+    print("Usage: () FILENAME COLUMN [ERR_COLUMN]".format(sys.argv[0]))
     exit()
+try:
+    err_columns = list(map(int,sys.argv[3].split(',')))
+except IndexError:
+    err_columns = None
 
 with open(filename, 'r') as data_file:
     reference_data = {}
@@ -24,11 +27,14 @@ with open(filename, 'r') as data_file:
             if not any(reference_data.values()):
                 for column in columns:
                     reference_data[column] = float(split[column])
-                for err_column in err_columns:
-                    reference_data[err_column] = float(split[err_column])
+                if err_columns:
+                    for err_column in err_columns:
+                        reference_data[err_column] = float(split[err_column])
+                print('ref: ',' '.join(map(str,split)))
             else:
                 for column in columns:
                     split[column] = float(split[column]) - reference_data[column]
-                for err_column in err_columns:
-                    split[err_column] = math.sqrt(float(split[err_column])**2 + reference_data[err_column]**2)
+                if err_columns:
+                    for err_column in err_columns:
+                        split[err_column] = math.sqrt(float(split[err_column])**2 + reference_data[err_column]**2)
                 print(' '.join(map(str,split)))
