7af3fd17cd720895dc430aa742cff40c45db82c0
giprayogo <genki016@gmail.com>
2019-02-19 14:40:02 +0900

    dp not copy atomic species from template; use argparse in templating_pwx.py

--- quickbin/templating_pwx.py
+++ quickbin/templating_pwx.py
@@ -9,55 +9,27 @@ import sys
 import shutil
 import glob
 
+import argparse
+
 import m_module.fileio as fileio
 
 np.set_printoptions(precision=15)
 script = os.path.basename(__file__)
 
 # read input files
-try:
-    template = sys.argv[1]
-    structure = sys.argv[2]
-except IndexError:
-    print(script+": Usage: {} TEMPLATE_INPUT STRUCTURE_FILE".format(sys.argv[0]))
-    exit()
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--template-file', required=True,
+        help='template pw.x input file with all simulation parameters')
+parser.add_argument('-s', '--structure-file', required=True,
+        help='pw.x input file with final structure')
 
-# force availability of job submit files
-try:
-    username = open('UserName', 'r')
-    print(username.readlines())
-    username.close()
-except IOError:
-    print(script+": UserName: No such file")
-    exit()
-try:
-    jobname = open('JobName', 'r')
-    print(jobname.readlines())
-    jobname.close()
-except IOError:
-    print(script+": JobName: No such file")
-    exit()
-try:
-    pp = open('Si.bfd.upf', 'r')
-    pp.close()
-except IOError:
-    print(script+": Si.bfd.upf: No such file")
-    exit()
-try:
-    pp = open('C.BFD.upf', 'r')
-    pp.close()
-except IOError:
-    print(script+": C.bfd.upf: No such file")
-    exit()
-try:
-    pp = open('H.BFD.upf', 'r')
-    pp.close()
-except IOError:
-    print(script+": H.bfd.upf: No such file")
-    exit()
+parsed = parser.parse_args()
+template = parsed.template_file
+structure = parsed.structure_file
 
 pwi = fileio.parse_pwx(template)
 pws = fileio.parse_pwx(structure)
+
 # copy structure file parameters to template
 pwi['celldm(1)']['value'] = pws['celldm(1)']['value']
 pwi['nat']['value'] = pws['nat']['value']
@@ -66,6 +38,6 @@ pwi['cell_parameters']['value'] = pws['cell_parameters']['value']
 pwi['cell_parameters']['options'] = pws['cell_parameters']['options']
 pwi['atomic_positions']['value'] = pws['atomic_positions']['value']
 pwi['atomic_positions']['options'] = pws['atomic_positions']['options']
-pwi['atomic_species'] = pws['atomic_species']
+#pwi['atomic_species'] = pws['atomic_species']
 
 print(fileio.tostring_pwx(pwi))
