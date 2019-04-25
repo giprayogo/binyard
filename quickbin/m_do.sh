4983a7a3b5aeffbf8caaa106d9889c13749899c5
giprayogo <genki016@gmail.com>
2019-04-25 15:35:39 +0900

    Split file and directory handling of m_do.sh
    On Apr 25, 2019

--- quickbin/m_do.sh
+++ quickbin/m_do.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
-# exec command (last arg) on each dir (other args)
-# (for doing equivalent to e.g. tossall, fetchall)
+#!/bin/bash -i
+# tossall, fetchall, -any-all replacement
+# eval command (last arg) on each dir (other args)
+# 2018/04/23: Use subshell by replacing eval with bash -c
+# 2018/04/23: Finally found suitable implementation
+# 2018/04/23: Revert to directory limit; some generalization is required for file operations
+# 2018/04/23: Do not restrict to directories; file operations are sometimes desired
 # 2018/09/13: First composed by genki
-[ -z "$1" ] && { echo "usage: ${0##*/} DIRECTORIES* COMMAND"; exit; }
-for dir in "${@:1:(($#-1))}"
+# sometime in April 19: directory only, make aliases work by using interactive -i
+[ -z "$1" ] && { echo "usage: ${0##*/} FILES/DIRS* COMMAND"; exit; }
+for file in "${@:1:(($#-1))}"
 do
- cwd=$(pwd)
- cd $dir
-  eval "${@: -1}"
- cd $cwd
+ [ -d $file ] && { cwd=$(pwd); cd $file; bash -c "${@: -1}"; cd $cwd; }
+ [ -f $file ] && bash -c "${@: -1} $file"
 done
