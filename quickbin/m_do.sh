2460867562cdb4fce9d356e90508c7b7a5f8f7cd
giprayogo <genki016@gmail.com>
2019-04-25 22:19:19 +0900

    Little changes to m_do.sh

--- quickbin/m_do.sh
+++ quickbin/m_do.sh
@@ -1,12 +1,9 @@
-#!/bin/bash
+#!/bin/bash -i
 # exec command (last arg) on each dir (other args)
 # (for doing equivalent to e.g. tossall, fetchall)
 # 2018/09/13: First composed by genki
 [ -z "$1" ] && { echo "usage: ${0##*/} DIRECTORIES* COMMAND"; exit; }
 for dir in "${@:1:(($#-1))}"
 do
- cwd=$(pwd)
- cd $dir
-  eval "${@: -1}"
- cd $cwd
+ [ -d $dir ] && { cwd=$(pwd); cd $dir; eval ${@: -1}; cd $cwd; }
 done
