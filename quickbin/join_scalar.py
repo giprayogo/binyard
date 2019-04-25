d7f85817af3350e1a8632a125070377448464bcb
giprayogo <genki016@gmail.com>
2019-04-25 22:25:08 +0900

    Structure and variable name cleanup
    On Apr 25, 2019

--- quickbin/join_scalar.py
+++ quickbin/join_scalar.py
@@ -2,6 +2,7 @@
 import re
 import sys
 import argparse
+#import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('files', nargs='+')
@@ -11,27 +12,32 @@ def resolve(regex_match):
     if regex_match:
         return regex_match.group(0)
     return ''
-grouped = {}
-for filename in parsed.files:
-    twist = resolve(re.search(r'tw[0-9]+', filename))
-    try:
-        grouped[twist].append(filename)
-    except KeyError:
-        grouped[twist] = []
-        grouped[twist].append(filename)
 
 def sort_by_series(x):
     return resolve(re.search(r'\.s[0-9]+\.', filename)).strip('.').strip('s')
-for twist in grouped.keys():
-    fn_list = grouped[twist]
-    fn_list.sort(key=sort_by_series)
-    out_name = re.sub(r'\.s[0-9]+\.', r'.s000.', fn_list[0])
+
+
+scalar = {}
+for filename in parsed.files:
+    twist = resolve(re.search(r'tw[0-9]+', filename))
+    try:
+        scalar[twist].append(filename)
+    except KeyError:
+        scalar[twist] = []
+        scalar[twist].append(filename)
+
+print(scalar)
+
+for twist in scalar.keys():
+    filename_list = scalar[twist]
+    filename_list.sort(key=sort_by_series)
+    out_name = re.sub(r'\.s[0-9]+\.', r'.s000.', filename_list[0])
     out_string = ''
     # dirty quick method
-    with open(fn_list[0]) as header_ref:
+    with open(filename_list[0]) as header_ref:
         out_string += header_ref.readlines()[0]
-    for fn in fn_list:
-        with open(fn, 'r') as something:
+    for filename in filename_list:
+        with open(filename, 'r') as something:
             some_list = [ x for x in something.readlines() if not '#' in x ]
             out_string += '\n'.join(some_list)
     with open(out_name, 'w') as something:
