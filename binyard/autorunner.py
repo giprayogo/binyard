1e1f560a63b35b445c3cb8bd23d2940c3a887d42
giprayogo <genki016@gmail.com>
2019-06-10 15:07:43 +0900

    Added new style autorunner, Autorunner2

--- binyard/autorunner.py
+++ binyard/autorunner.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # autorunning helpers
 # structure not finalized!
-import threading
+from __future__ import print_function
 from threading import Timer
 import time
 import re
 import fnmatch
 import os
+import sys
+import signal
 
 
 # Exit clauses, implemented as Exceptions
@@ -24,7 +26,7 @@ class EnoughSampleError(Exception):
 
 
 # Wrappers
-# TODO: move under debugging
+# TODO: move under its own file
 # Remember to RETURN the functions
 # only accept single *args!
 # default 'old' to be older than 1 hour
@@ -53,8 +55,6 @@ def not_old(_func=None, threshold=7500):
         return oldchecking
     else:
         return oldchecking(_func)
-
-
 
 
 def latest(_=None, pattern=''):
@@ -93,6 +93,7 @@ def dmc_dat(_=None, start=5):
 
 
 # Used for sorting
+# TODO: should these be grouped together?
 def series(filename):
     """Get series number from scalar filename"""
     return resolve(re.search(r'\.s[0-9]+\.', filename)).strip('.').strip('s')
@@ -103,11 +104,12 @@ def twist(filename):
 
 
 # to exit, sensor function must raise Exceptions
-class Autorunner():
+class Autorunner(object):
     def __init__(self, sensor, actuator):
         self.sensor = sensor # callable, return boolean
         self.actuator = actuator # callable, return not checked
 
+    @print_args
     def run(self, interval=None):
         if interval is None:
             interval = self.interval
@@ -115,7 +117,59 @@ class Autorunner():
         if self.sensor():
             self.actuator()
         print('waiting {0} seconds'.format(interval))
-        threading.Timer(interval, self.run).start()
+        Timer(interval, self.run).start()
+
+# the way to use it is by EXTENDING this class
+class Autorunner2(object):
+    # things you will EXTEND/OVERWRITE
+    def __init__(self, interval, outstream=None, errstream=sys.stderr):
+        self.interval = interval
+        self.outstream = outstream
+        signal.signal(signal.SIGINT, self._quit)
+        signal.signal(signal.SIGTERM, self._quit)
+        self._printout('PID: {0}'.format(os.getpid()))
+
+    # sensor for actuation
+    def _sensor(self):
+        self._printerr('Not implemented')
+        return True
+
+    # what you are automatically doing
+    def _actuator(self):
+        self._printerr('Not implemented')
+        pass
+
+    # when exiting what do you want to do
+    @timestamp
+    def _terminator(self):
+        pass
+
+    def _quit(self, signum=1, frame=None):
+        self._terminator()
+        sys.exit(signum)
+
+    def run(self):
+        try:
+            if self._sensor():
+                self._actuator()
+        except Exception as e:
+            self._terminator()
+            raise
+        self._printout('waiting {0} seconds'.format(self.interval))
+        Timer(self.interval, self.run).start()
+
+
+    # things you can use
+    def _printout(self, message):
+        if self.outstream is None:
+            print(message, file=sys.stdout)
+        else:
+            with open(self.outstream, 'a') as fh:
+                print(message, file=fh)
+
+    def _printerr(self, message):
+        print(message, file=self.errstream)
+
 
 def done_cobaltlog(filename):
     #print('cobalt log: {0}'.format(filename))
