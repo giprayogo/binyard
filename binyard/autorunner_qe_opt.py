02108c956b8648900f30579da87790fdf64dbd48
giprayogo <genki016@gmail.com>
2019-06-10 15:05:17 +0900

    Add autorunner for QE on unified

---
+++ binyard/autorunner_qe_opt.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# autorunning qe relax/vc-relax
+
+from binyard.autorunner import Autorunner2
+from binyard.autorunner import print_args
+from binyard.autorunner import timestamp
+from binyard.fileio import parse
+import os
+import re
+import fnmatch
+import subprocess
+import inspect
+import argparse
+import sys
+
+class QEAutorunner(Autorunner2):
+    # non-changable class variables
+    _OPTIMIZED = 'atomic positions/cell parameters has been optimized'
+    _FAIL_SUBMIT = 'qsub failure'
+    _QE_CRASH = 'QE has encountered an error'
+    _RUNNING = 'job is running'
+    _FINISHED = 'job has been finished'
+    _COUNT_FORMAT = 'current count={0}'
+
+    # super-inherited functions
+    # constructor; the outstream was originally implemented due to annoying redirection behaviour
+    # when working with a perl script (reason still unknown)
+    def __init__(self, toss_args=None, count=0, *args, **kwargs):
+        super(QEAutorunner, self).__init__(*args, **kwargs)
+        if toss_args is None:
+            raise ValueError("must set toss arguments")
+        self.toss_args = toss_args
+        self.count = count
+
+    # sensing for re-submit
+    def _sensor(self):
+        done = False
+        try:
+            self.fetch()
+            if self.job_finished():
+                super(QEAutorunner, self)._printout(self._FINISHED)
+                done = True
+            else:
+                super(QEAutorunner, self)._printout(self._RUNNING)
+        except Exception:
+            raise
+        return done
+
+    @timestamp
+    def _terminator(self, *args, **kwargs):
+        # do not fetch since some files may get deleted
+        self.qdel()
+        super(QEAutorunner, self)._terminator(*args, **kwargs)
+
+    @timestamp
+    def _actuator(self):
+        self.update_files()
+        self.toss()
+
+    # child-only functions
+    # public
+    def qdel(self):
+        super(QEAutorunner, self)._printout('qdel')
+        subprocess.call(['qdel'])
+
+    # TODO: I need to define correct behaviour in case of qstat
+    # there is curently one hole in which output file is not created,
+    # and everything stops, but no crash informations.
+    #def qs(self):
+    #    cluster = self.toss_args.split('.')[0]
+    #    with open('JobNumber', 'r') as fh:
+    #        jobnumber = fh.read().strip()
+    #    qs = subprocess.call(['qs', cluster])
+
+    def toss(self):
+        super(QEAutorunner, self)._printout(self.toss_args)
+        subprocess.call(['m_toss.pl', self.toss_args])
+
+    def fetch(self):
+        #TODO: somehow generalize this
+        cmd = ['m_fetch.py', '--exclude', 'out', '--exclude', '*.log']
+        super(QEAutorunner, self)._printout(' '.join(cmd))
+        subprocess.call(cmd)
+
+    def update_files(self):
+        self.count += 1
+        print(self.count)
+        super(QEAutorunner, self)._printout(self._COUNT_FORMAT.format(self.count))
+        subprocess.call(['extract_final.py'])
+        # TODO: make these as user input
+        print(' '.join(['mv', 'input.in', 'input.in.'+str(self.count)]))
+        subprocess.call(['mv', 'input.in', 'input.in.'+str(self.count)])
+        subprocess.call(['mv', 'out.o', 'out.o.'+str(self.count)])
+        subprocess.call(['mv', 'auto_final.in', 'input.in'])
+
+    @print_args
+    def job_finished(self):
+        done = False
+        #TODO: instead of simply out, read qe's outdir
+        try:
+            if os.stat('JobNumber').st_size == 0:
+                raise Exception(self._FAIL_SUBMIT)
+        except IOError:
+            # in any case if there are no JobNumber the toss is presumed to has been failed
+            raise Exception(self._FAIL_SUBMIT)
+        if 'CRASH' in os.listdir('.'):
+            raise Exception(self._QE_CRASH)
+        try:
+            with open('out.o', 'r') as fh:
+                text = fh.read()
+                if 'stopping ...' in text:
+                    raise Exception(self._QE_CRASH)
+                if 'Begin final coordinates' in text:
+                    raise Exception(self._OPTIMIZED)
+                if 'JOB DONE' in text:
+                    done = True
+        except IOError:
+            # TODO: also check job queues
+            pass # if job is queued; there will not be any output files
+        super(QEAutorunner, self)._printout('job_finished return as : {0}'.format(done))
+        return done
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('toss_string', type=str, default=None, nargs='?')
+    parser.add_argument('--interval', '-i', type=int, required=True)
+    parser.add_argument('--count', '-c', type=int, default=0)
+    parser.add_argument('--logfile', '-l', default=None)
+    args = parser.parse_args()
+
+    # sometimes we want to process the arguments; this is a perhaps better practice
+    toss_string = args.toss_string
+    interval = args.interval
+    count = args.count
+    logfile = args.logfile
+
+    if toss_string is None:
+        jsses = [ x for x in os.listdir('.') if fnmatch.fnmatch(x, '*.jss') ]
+        assert len(jsses) == 1
+        toss_string = jsses[0].replace('.jss', '')
+    auto = QEAutorunner(toss_string, count, interval=interval, outstream=logfile)
+    # do initial tosses
+    auto.toss()
+    auto.run()
