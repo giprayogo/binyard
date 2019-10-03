a7a1dc606394eef1697bf819d035afbb149651c9
giprayogo <genki016@gmail.com>
2019-10-03 15:41:51 +0900

    Correct ordering for density reading

--- binyard/density_diff.py
+++ binyard/density_diff.py
@@ -14,6 +14,7 @@ class QEScalarField(object):
         self.comment = comment
         self.header = header
         self.data = data
+        self.dr = dr
         pass
 
     @classmethod
@@ -31,13 +32,16 @@ class QEScalarField(object):
         dx = float(numerical_data[1].split()[1])
         dy = float(numerical_data[2].split()[2])
         dz = float(numerical_data[3].split()[3])
+        # TODO: using proper grid array in place of dr
+        # currently will only work on cube cells
 
         # yes this is correct regardless of what
         header = numerical_data[:4+nat]
 
         # first write in form of long vector, and just reshape according to the definitions in the file
         data = np.array(list(map(float, [ y for x in numerical_data[4+nat:] for y in x.split() ])))
-        data = np.reshape(data, [nz, ny, nx]) # note the order
+        #data = np.reshape(data, [nz, ny, nx]) # note the order
+        data = np.reshape(data, [nx, ny, nz]) # note the order
 
         # Real dimension?
         #z = 0
@@ -46,88 +50,70 @@ class QEScalarField(object):
         qescalarfield = cls(comment, header, data, (dx,dy,dz))
         return qescalarfield
 
-    def to_file(self, filename):
+    def to_file(self, filename, datatype='charge'):
         """ Assuming the charge in form of 3D-numpy array """
-        data = np.reshape(self.data, (int(self.data.size/6), 6))
+        assert datatype == 'charge' or datatype == 'rdg' or datatype == 'gradient'
         with open(filename, 'w') as fh:
             for line in self.comment:
                 fh.write(line)
             for line in self.header:
                 fh.write(line)
-            #print(cube['data'].shape[0])
-            fh.write('\n'.join([ ' '.join([ str(y) for y in x ]) for x in data ]))
+            if datatype == 'charge':
+                data = np.reshape(self.data, (int(self.data.size/6), 6))
+                fh.write('\n'.join([ ' '.join([ str(y) for y in x ]) for x in data ]))
+            elif datatype == 'rdg':
+                data = np.reshape(self.rdg, (int(self.rdg.size/6), 6))
+                fh.write('\n'.join([ ' '.join([ str(y) for y in x ]) for x in data ]))
+            elif datatype == 'gradient':
+                data = np.reshape(self.gradient, (int(self.gradient.size/6), 6))
+                fh.write('\n'.join([ ' '.join([ str(y) for y in x ]) for x in data ]))
 
     def get_gradient(self):
         # TODO: scale according to the grid, + save
-        self.gradient = np.gradient(self.data, self.dr)
+        # misleading name; calculate the ABSOLUTE gradient; will change later
+        gradients = np.gradient(self.data, *self.dr)
+        print(self.data.size, gradients[0].size)
+        self.gradient = np.sqrt(
+                np.power(gradients[0],2) +
+                np.power(gradients[1],2) +
+                np.power(gradients[2],2))
+        print(self.gradient.size)
+        print(self.gradient.shape)
         return self.gradient
 
     def get_reduced_gradient(self):
         """ Reduced density gradient (RDG); often used in context of non-covalent interactions """
-        return np.abs(self.gradient) / ( self.gradient**(4./3.)*2*(3*np.pi**2)**(1/3.) )
-
-
-
-
-# TODO: don't like this manual style; but not essential
-# and not deterimental to the performance either
-def read_charge(density_file):
-    cube = defaultdict(list)
-    with open(density_file, 'r') as xx:
-        # hand-handled
-        lines = xx.readlines()
-    comment = lines[:2]
-    numerical_data = lines[2:]
-    nat = int(numerical_data[0].split()[0])
-    nx = int(numerical_data[1].split()[0])
-    ny = int(numerical_data[2].split()[0])
-    nz = int(numerical_data[3].split()[0])
-    dx = float(numerical_data[1].split()[1])
-    dy = float(numerical_data[2].split()[2])
-    dz = float(numerical_data[3].split()[3])
-
-    # yes this is correct regardless of what
-    cube['comment'] = comment
-    cube['header'] = numerical_data[:4+nat]
-    cube['data'] = np.array(list(map(float, [ y for x in numerical_data[4+nat:] for y in x.split() ])))
-    cube['data'] = np.reshape(cube['data'], (int(cube['data'].shape[0]/6), 6))
-    #big_mat = np.zeros([nx, ny, nz])
-    #big_mat = np.reshape(big_vect, [nz, ny, nx]) # note the order
-    #print(big_mat.shape, big_mat[0][0][:10])
-    # dimension?
-    #z = 0
-    #zi = int(z/dz)
-    #print(zi)
-    return cube
-
-def write_cube(filename, data):
-    """ Assuming the charge in form of 3D-numpy array """
-    with open(filename, 'w') as fh:
-        for line in data['comment']:
-            fh.write(comment)
-        for line in data['header']:
-            fh.write(line)
-        #print(cube['data'].shape[0])
-        fh.write('\n'.join([ ' '.join([ str(y) for y in x ]) for x in data['data']]))
+        self.rdg = self.gradient / ( np.power(self.data, 4./3.)*2*(3*np.pi**2)**(1./3.) )
+        # test each term
+        # self.rdg = self.gradient / np.power(self.data, 4./3.)
+        print(self.rdg.size)
+        print(self.rdg.shape)
+        return self.rdg
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--output-filename', '-o', default='delta.cube')
-    parser.add_argument('density_files', nargs=2)
+    parser.add_argument('--type', '-t', type=str, default='delta',
+            choices=['delta', 'gradient', 'rdg', 'y-gradient'])
+    parser.add_argument('density_files', nargs='+')
     args = parser.parse_args()
-    density_files = args.density_files
-    #density0 = read_charge(density_files[0])
-    #density1 = read_charge(density_files[1])
-    density0 = QEScalarField.from_file(density_files[0])
-    density1 = QEScalarField.from_file(density_files[1])
-    #assert density0['header'] == density1['header']
-
-    #density_delta = defaultdict(list)
-    density_delta = copy.deepcopy(density0)
-    #density_delta['comment'] = density0['comment']
-    #density_delta['header'] = density0['header']
-    #density_delta['data'] = density0['data'] - density1['data']
-    density_delta.data = density0.data - density1.data
-    #write_cube(args.output_filename, density_delta)
-    density_delta.to_file(args.output_filename)
+    if args.type == 'delta':
+        assert(len(args.density_files)) == 2
+        density_files = args.density_files
+        density0 = QEScalarField.from_file(density_files[0])
+        density1 = QEScalarField.from_file(density_files[1])
+        density_delta = copy.deepcopy(density0)
+        density_delta.data = density0.data - density1.data
+        density_delta.to_file(args.output_filename)
+    elif args.type == 'rdg':
+        assert(len(args.density_files)) == 1
+        density = QEScalarField.from_file(args.density_files[0])
+        density.get_gradient()
+        density.get_reduced_gradient()
+        density.to_file(args.output_filename, 'rdg')
+    elif args.type == 'gradient': # actually norm-gradient (scalar)
+        assert(len(args.density_files)) == 1
+        density = QEScalarField.from_file(args.density_files[0])
+        density.get_gradient()
+        density.to_file(args.output_filename, 'gradient')
