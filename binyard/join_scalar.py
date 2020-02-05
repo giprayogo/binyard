5f80afc30f7c91398699972c8cb667a88d871c39
giprayogo <genki016@gmail.com>
2020-02-05 18:17:23 +0900

    Removed unecessary components

--- binyard/join_scalar.py
+++ binyard/join_scalar.py
@@ -17,7 +17,11 @@ def output_capturer(x):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('files', nargs='*', default=common())
-parsed = parser.parse_args()
+parser.add_argument('-e', '--equil', nargs='*', type=int, default=None)
+args = parser.parse_args()
+if not args.equil:
+    args.equil = [0] * len(args.files)
+assert len(args.files) == len(args.equil)
 
 def resolve(regex_match):
     if regex_match:
@@ -31,10 +35,13 @@ def sort_by_series(x):
 if __name__ == '__main__':
     # divide files by number of twists
     scalar = {}
-    for filename in parsed.files:
+    equil_scalar = {}
+    for e, filename in zip(args.equil, args.files):
         twist = resolve(re.search(r'tw[0-9]+', filename))
         scalar.setdefault(twist, [])
+        equil_scalar.setdefault(twist, [])
         scalar[twist].append(filename)
+        equil_scalar[twist].append(e)
 
     # (TEMP) calculate the total of multiple runs
     qmcout = []
@@ -46,34 +53,20 @@ if __name__ == '__main__':
         qmcout.append(filename)
 
     print(scalar)
-    #print(qmcout)
-    #print([ len(qmcout[x]) for x in qmcout.keys() ])
-    dmc_times = [ float(y.strip().split()[4])
-        for x in qmcout
-        for y in open(x, 'r').readlines()
-        if 'QMC Execution time' in y ]
-    mpi_ranks = [ y
-        for x in qmcout
-        for y in open(x, 'r').readlines()
-        if 'Total number of MPI ranks' in y ]
-    omp_threads = [ y
-        for x in qmcout
-        for y in open(x, 'r').readlines()
-        if 'OMP 1st level threads' in y ]
-
-    print('Total DMC time: {}'.format(dmc_time))
 
     for twist in scalar.keys():
         filename_list = scalar[twist]
+        eq_length_list = equil_scalar[twist]
         filename_list.sort(key=sort_by_series)
         out_name = re.sub(r'\.s[0-9]+\.', r'.s000.', filename_list[0])
         out_string = ''
         # dirty quick method
-        with open(filename_list[0]) as header_ref:
+        with open(filename_list[0]) as header_ref: # separate the header
             out_string += header_ref.readlines()[0]
-        for filename in filename_list:
+        for eq_length, filename in zip(eq_length_list, filename_list):
             with open(filename, 'r') as something:
                 some_list = [ x.strip() for x in something.readlines() if not '#' in x ]
+                some_list = some_list[eq_length:]
                 out_string += '\n'.join(some_list).strip() + '\n'
         with open(out_name, 'w') as something:
             something.write(out_string)
