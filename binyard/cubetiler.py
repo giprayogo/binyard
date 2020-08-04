371be7e1d8e18e9a89940b9ba1ba02cad154d151
giprayogo <genki016@gmail.com>
2020-08-04 16:32:10 +0900

    Finished cubetiler

--- binyard/cubetiler.py
+++ binyard/cubetiler.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
-"""CUBE."""
+"""Tiling of CUBE with trilinear interpolation."""
 import argparse
+#import time
+from itertools import product
 import numpy as np
-from numpy.linalg import norm
+from numpy.linalg import norm, inv
+from numba import jit
 
 class Cube():
     """Gaussian CUBE file.
 
     Format reference: http://paulbourke.net/dataformats/cube/"""
-    def __init__(self, comments, nat, origin, grid, voxel, unit, species, charges, coordinates, volumetrics):
+    def __init__(self, comments, nat, origin, grid, voxel, unit, species, charges,
+                 coordinates, volumetrics):
         self.comments = comments
         self.nat = nat
         self.origin = origin
@@ -20,6 +24,10 @@ class Cube():
         self.coordinates = coordinates
         self.volumetrics = volumetrics
 
+        # Derived stuffs.
+        self.cell = (self.voxel.T * self.grid).T
+        self.icell = inv(self.cell)
+
     @classmethod
     def from_file(cls, filename):
         """From file."""
@@ -27,61 +35,190 @@ class Cube():
         with open(filename, 'r') as f:
             lines = f.readlines()
         # Headers.
-        comments = line[:2]
-        natorigin = np.array(line[2])
-        nat = natorigin[0]
+        comments = [x.strip() for x in lines[:2]]
+        natorigin = np.fromstring(lines[2], sep=' ')
+        nat = natorigin[0].astype(np.int64)
         origin = natorigin[1:]
-        voxelgrid = np.array(line[3:6])
-        grid = np.abs(voxelgrid[:, 0])
+        voxelgrid = Cube.strit2np2d(lines[3:6])
+        grid = np.abs(voxelgrid[:, 0]).astype(np.int64)
         voxel = voxelgrid[:, 1:]
         unit = 'bohr' if (voxelgrid[:, 0] > 0).all() else 'angstrom'
-        scc = np.fromstring(line[6:6+nat])
-        species = scc[:, 0]
-        charges = scc[:, 1]
+        scc = Cube.strit2np2d(lines[6:6+nat])
+        species = scc[:, 0].astype(np.int64)
+        charges = scc[:, 1].astype(np.int64)
         coordinates = scc[:, 2:]
-        volumetrics = np.fromstring(line[6+nat:])
-        return cls(comments, nat, origin, grid, voxel, unit, species, charges, coordinates, volumetrics)
+        volumetrics = Cube.strit2np2d(lines[6+nat:])
+        volumetrics = np.reshape(volumetrics, grid)
+        return cls(comments, nat, origin, grid, voxel, unit, species, charges,
+                   coordinates, volumetrics)
 
     def __str__(self):
         string = ''
-        for _ in comments:
+        for _ in self.comments:
             string += _
             string += '\n'
-        string += str(nat)
-        string += ' '.join(map(str, origin))
+        string += str(self.nat)
+        string += ' '
+        string += ' '.join(map(str, self.origin))
         string += '\n'
-        string += self.np2d2str(np.hstack((self.grid, self.voxel)))
-        string += self.np2d2str(np.hstack((self.species, self.charges, self.coordinates)))
-        string += self.np2d2str(self.volumetrics)
-        return string
-
-    #@jit(nopython=True)
-    @staticmethod
-    def np2d2str(array):
-        string = ''
-        for np1d in array:
-            string += ' '.join(map(str, np1d))
-            string += '\n'
+        string += self.np2d2str(np.hstack((np.reshape(self.grid, (3, 1)), self.voxel)),
+                                fmt='{:.0f} {:.15f} {:.15f} {:.15f}')
+        string += self.np2d2str(np.hstack((np.reshape(self.species, (self.species.shape[0], 1)),
+                                           np.reshape(self.charges, (self.charges.shape[0], 1)),
+                                           self.coordinates)),
+                                fmt='{:.0f} {:.0f} {:.15f} {:.15f} {:.15f}')
+        string += self.np2d2str(np.reshape(self.volumetrics, (-1, 6)))
         return string
 
     def to_file(self, filename):
+        """Well what else."""
         with open(filename, 'w') as f:
             f.write(str(self))
 
+    @staticmethod
+    def np2d2str(array, fmt=None):
+        """Numpy 2d to string."""
+        string = ''
+        if fmt is None:
+            for np1d in array:
+                string += ' '.join(map(str, np1d))
+                string += '\n'
+            return string
+        for np1d in array:
+            string += fmt.format(*np1d)
+            string += '\n'
+        return string
+
+    @staticmethod
+    def strit2np2d(iterable):
+        """String iterable to 2d Numpy array."""
+        array = []
+        for _ in iterable:
+            array.append(np.fromstring(_, sep=' '))
+        return np.array(array)
+
+    @staticmethod
+    def lte(a, b, rtol=1e-05, atol=1e-08, equal_nan=False):
+        """Less than equal for floats."""
+        return (a <= b).all() or np.allclose(a, b, rtol=rtol, atol=atol, equal_nan=equal_nan)
+
+    @staticmethod
+    def gte(a, b, rtol=1e-05, atol=1e-08, equal_nan=False):
+        """Greater than equal for floats."""
+        return (a >= b).all() or np.allclose(a, b, rtol=rtol, atol=atol, equal_nan=equal_nan)
+
+    @staticmethod
+    def vol(cell):
+        """Volume enclosed by 3-vector."""
+        return np.abs(np.dot(np.cross(cell[0], cell[1]), cell[2]))
+
+    @staticmethod
+    @jit(nopython=True)
+    def tl_interpolate(p, grid, volumetric):
+        """Trilinear interpolation of point values to the original point."""
+        # Map point to primitive cell, in "grid" basis
+        # lower and upper
+        p0 = np.floor(p)
+        p1 = p0+1.
+        # Only for taking volumetric's values
+        pm0 = np.mod(p0, grid).astype(np.int64)
+        pm1 = np.mod(p1, grid).astype(np.int64)
+        mat = np.array([
+            [1, p0[0], p0[1], p0[2], p0[0]*p0[1], p0[0]*p0[2], p0[1]*p0[2], p0[0]*p0[1]*p0[2]],
+            [1, p1[0], p0[1], p0[2], p1[0]*p0[1], p1[0]*p0[2], p0[1]*p0[2], p1[0]*p0[1]*p0[2]],
+            [1, p0[0], p1[1], p0[2], p0[0]*p1[1], p0[0]*p0[2], p1[1]*p0[2], p0[0]*p1[1]*p0[2]],
+            [1, p1[0], p1[1], p0[2], p1[0]*p1[1], p1[0]*p0[2], p1[1]*p0[2], p1[0]*p1[1]*p0[2]],
+            [1, p0[0], p0[1], p1[2], p0[0]*p0[1], p0[0]*p1[2], p0[1]*p1[2], p0[0]*p0[1]*p1[2]],
+            [1, p1[0], p0[1], p1[2], p1[0]*p0[1], p1[0]*p1[2], p0[1]*p1[2], p1[0]*p0[1]*p1[2]],
+            [1, p0[0], p1[1], p1[2], p0[0]*p1[1], p0[0]*p1[2], p1[1]*p1[2], p0[0]*p1[1]*p1[2]],
+            [1, p1[0], p1[1], p1[2], p1[0]*p1[1], p1[0]*p1[2], p1[1]*p1[2], p1[0]*p1[1]*p1[2]]])
+        c = np.array([
+            volumetric[pm0[0], pm0[1], pm0[2]],
+            volumetric[pm1[0], pm0[1], pm0[2]],
+            volumetric[pm0[0], pm1[1], pm0[2]],
+            volumetric[pm1[0], pm1[1], pm0[2]],
+            volumetric[pm0[0], pm0[1], pm1[2]],
+            volumetric[pm1[0], pm0[1], pm1[2]],
+            volumetric[pm0[0], pm1[1], pm1[2]],
+            volumetric[pm1[0], pm1[1], pm1[2]]])
+        a = inv(mat) @ c
+        return a[0] + a[1]*p[0] + a[2]*p[1] + a[3]*p[2] + \
+            a[4]*p[0]*p[1] + a[5]*p[0]*p[2] + a[6]*p[1]*p[2] + \
+            a[7]*p[0]*p[1]*p[2]
+
+    @staticmethod
+    def nn_interpolate(p, grid, volumetric):
+        """Nearest neighbor interpolation of point values to the original point."""
+        # Map point to primitive cell, in "grid" basis
+        pp = np.mod(np.round(p).astype(int), grid)
+        return volumetric[tuple(pp)]
+
+    @staticmethod
+    def fill(fn, grid, ngrid, voxel, volumetric, icell):
+        """Fill grid with fn values."""
+        a = np.empty(ngrid)
+        for k in product(range(ngrid[0]), range(ngrid[1]), range(ngrid[2])):
+            point = ((voxel.T * k).T.sum(axis=0) @ icell) * grid
+            a[k] = fn(point, grid, volumetric)
+        return a
+
     def tile(self, tilemat):
-        """Resize the unit cell by the specified tiling matrix."""
-        # Decide new voxel size. Keep voxel volume.
-        pass
+        """Resize the unit cell by the specified tiling matrix. In-place."""
+        scale = np.abs(np.linalg.det(tilemat))
+        # Scale unit cell.
+        self.nat *= scale
+        cellp = tilemat @ self.cell
+        fracsp = self.coordinates @ inv(cellp)
+
+        it = inv(tilemat) # cell in cellp des.
+
+        # Atomic coordinate tiling. Inefficient quick way.
+        nm = tilemat.max().astype(int)
+        _coords = []
+        _species = []
+        _charges = []
+        for r, s, c in zip(fracsp, self.species, self.charges):
+            for ijk in product(range(nm), repeat=3):
+                thing = r + (ijk * it.T).T.sum(axis=0)
+                if self.lte(thing, 1.0) and self.gte(thing, 0.):
+                    _coords.append((cellp.T @ thing.T).T)
+                    _species.append(s)
+                    _charges.append(c)
+        self.coordinates = np.array(_coords)
+        self.species = np.array(_species)
+        self.charges = np.array(_charges)
+
+        # Charge density tiling.
+        # Scale to keep voxel volume the same first,
+        # then choose nearest integer grid size.
+        #scalenb = (0.5 * self.vol(self.cell) / self.vol(cellp)) ** (1/3)
+        scalenb = (self.vol(self.cell) / self.vol(cellp)) ** (1/3)
+        nvoxel = scalenb * (self.voxel @ self.icell) @ cellp
+        # The voxel are parallel to the cell so we can do this.
+        ngrid = (norm(cellp, axis=1) / norm(nvoxel, axis=1)).round().astype(int)
+        # Resize the voxel for an integer grid
+        nvoxel = (cellp.T / ngrid).T
+        #start = time.time()
+        nvolumetric = self.fill(self.tl_interpolate, self.grid, ngrid,
+                                nvoxel, self.volumetrics, self.icell)
+        #end = time.time()
+        self.volumetrics = nvolumetric
+        self.grid = ngrid
+        self.voxel = nvoxel
 
 def main():
+    """Main."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('cubefile', help='Gaussian-style CUBE file')
+    parser.add_argument('cubefile', help="Gaussian-style CUBE file")
+    parser.add_argument('tilemat', help="Tiling matrix")
     args = parser.parse_args()
+    tilemat = np.fromstring(args.tilemat, sep=' ')
+    tilemat = tilemat.reshape(3, 3)
 
     cubefile = args.cubefile
     cube = Cube.from_file(cubefile)
+    cube.tile(tilemat)
     print(cube)
 
 if __name__ == '__main__':
     main()
-
